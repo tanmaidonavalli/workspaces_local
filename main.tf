19c064c1074c217b659165094fb880904d14c77d
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-18 20:17:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = "~> 0.12"
   backend "s3" {
     key            = "terraform.tfstate"
-    bucket         = "application1-dev"
+    bucket         = "app1-dev"
     dynamodb_table = "terraform-state-lock-app1dev"
     region         = "us-east-1"
     encrypt        = true
