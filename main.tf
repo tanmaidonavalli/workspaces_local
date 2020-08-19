095e9a4c5f84733e26b53afa3a19c3ec16c0112f
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-18 20:23:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-terraform {
-  required_version = "~> 0.12"
-  backend "s3" {
-    key            = "terraform.tfstate"
-    bucket         = "app1-dev"
-    dynamodb_table = "terraform-state-lock-app1dev"
-    region         = "us-east-1"
-    encrypt        = true
-    workspaces { prefix = "app1-dev" }
-  }
-}
+#terraform {
+  #required_version = "~> 0.12"
+  #backend "s3" {
+    #key            = "terraform.tfstate"
+    #bucket         = "app1-dev"
+    #dynamodb_table = "terraform-state-lock-app1dev"
+    #region         = "us-east-1"
+    #encrypt        = true
+    #workspaces { prefix = "app1-dev" }
+  #}
+#}
